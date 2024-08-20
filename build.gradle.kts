e46fd8731b4f6d55a71737ab632f0bae5ddba3c6
Marvin Haase <marvin.sempa.haase@gmail.com>
2024-08-20 14:56:23 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "8.0.0"
+}
+
+group = "dev.marfien"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("com.github.tomas-langer.cli:cli-progress:1.0.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "dev.marfien.thelistener.TheListener"
+    }
+}
